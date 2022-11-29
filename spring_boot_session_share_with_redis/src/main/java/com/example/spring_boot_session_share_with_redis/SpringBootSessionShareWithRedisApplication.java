16abfa53fcf58313216719715288c13e118e523b
fesabelilla <fesabelilla@gmail.com>
2022-11-29 20:39:49 +0600

    Init commit

---
+++ spring_boot_session_share_with_redis/src/main/java/com/example/spring_boot_session_share_with_redis/SpringBootSessionShareWithRedisApplication.java
@@ -0,0 +1,13 @@
+package com.example.spring_boot_session_share_with_redis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootSessionShareWithRedisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootSessionShareWithRedisApplication.class, args);
+	}
+
+}
